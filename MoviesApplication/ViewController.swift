4170f6f96f7184b593686e1cc196e129b5b8e0e1
jovanaaloleska <jovana.loleska@students.finki.ukim.mk>
2021-07-19 14:49:49 +0200

    Initial commit

---
+++ MoviesApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApplication
+//
+//  Created by Jovana Loleska on 7/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
