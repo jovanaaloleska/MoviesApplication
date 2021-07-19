989e2dbec5b1af3a6468d455b8834583ed72a99a
jovanaaloleska <jovana.loleska@students.finki.ukim.mk>
2021-07-19 16:28:51 +0200

    Set the blurred subview with rounded corners on a Welcome scene

--- MoviesApplication/WelcomeViewController.swift
+++ MoviesApplication/WelcomeViewController.swift
@@ -11,6 +11,7 @@ import SnapKit
 class WelcomeViewController: UIViewController {
     
     var backgroundImageView: UIImageView!
+    var logInView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,23 @@ class WelcomeViewController: UIViewController {
         backgroundImageView = UIImageView()
         backgroundImageView.image = UIImage(named: "welcomeScreenBackground")
         
+        logInView = UIView()
+        if !UIAccessibility.isReduceTransparencyEnabled{
+            logInView.backgroundColor = .clear
+            
+            let blurrEffect = UIBlurEffect(style: .dark)
+            logInView = UIVisualEffectView(effect: blurrEffect)
+        } else {
+            logInView.backgroundColor = .black
+        }
+        
+        logInView.layer.cornerRadius = 20
+        logInView.layer.masksToBounds = true
+        logInView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+
         self.view.addSubview(backgroundImageView)
+        self.view.addSubview(logInView)
     }
     
     func setUpConstraints() {
@@ -30,6 +47,19 @@ class WelcomeViewController: UIViewController {
             make.width.equalTo(self.view.snp.width)
             make.height.equalTo(self.view.snp.height)
         }
+        logInView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.centerY)
+            make.width.equalTo(view)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+    }
+}
+extension UIView {
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
     }
 }
 
