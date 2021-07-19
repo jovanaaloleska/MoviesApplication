6b58d5edf8ffb8ebec2ad997b39a8c97dada00f4
jovanaaloleska <jovana.loleska@students.finki.ukim.mk>
2021-07-19 15:32:55 +0200

    Set the background image on a Welcome scene

--- MoviesApplication/AppDelegate.swift
+++ MoviesApplication/AppDelegate.swift
@@ -9,10 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
+    var navigationController: UINavigationController!
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        navigationController = UINavigationController(rootViewController: WelcomeViewController())
+        navigationController.navigationBar.isHidden = true
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
---
+++ MoviesApplication/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MoviesApplication
+//
+//  Created by Jovana Loleska on 7/19/21.
+//
+
+import UIKit
+import SnapKit
+
+class WelcomeViewController: UIViewController {
+    
+    var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+        setUpConstraints()
+        // Do any additional setup after loading the view.
+    }
+    func setUpViews() {
+        backgroundImageView = UIImageView()
+        backgroundImageView.image = UIImage(named: "welcomeScreenBackground")
+        
+        self.view.addSubview(backgroundImageView)
+
+    }
+    
+    func setUpConstraints() {
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view.snp.width)
+            make.height.equalTo(self.view.snp.height)
+        }
+    }
+}
+
